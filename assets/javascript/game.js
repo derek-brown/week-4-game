c402ffd6bd7d3d4a81486620e16280d2c471b5ac
derek-brown <derek.ethan.brown@gmail.com>
2017-08-07 23:04:59 -0400

    Game is working! Nonessential debugging needed on game resetting before the final value is displayed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,44 @@
 $(document).ready(function(){
-    var crystal1 = -1;
-    var crystal2 = -1;
-    var crystal3 = -1;
-    var crystal4 = -1;
+    var crystal1 = Math.floor(Math.random()*12)+1;
+    var crystal2 = Math.floor(Math.random()*12)+1;
+    var crystal3 = Math.floor(Math.random()*12)+1;
+    var crystal4 = Math.floor(Math.random()*12)+1;
+    var random = Math.floor(Math.random()*102)+19;
     var score = 0;
+    var win = 0;
+    var loss = 0;
 
+    function check(){
+        if (score === random){
+            win++;
+            $(".wins").html("Wins: "+win);
+            alert("You win!");
+            crystal1 = Math.floor(Math.random()*12)+1;
+            crystal2 = Math.floor(Math.random()*12)+1;
+            crystal3 = Math.floor(Math.random()*12)+1;
+            crystal4 = Math.floor(Math.random()*12)+1;
+            random = Math.floor(Math.random()*102)+19;
+            score = 0;
+            $(".rndNum").html(random);
+            $(".score").html(score);
+        }
+        else if(score >= random){
+            loss++;
+            $(".losses").html("Losses: "+loss);
+            alert("You lose!");
+            crystal1 = Math.floor(Math.random()*12)+1;
+            crystal2 = Math.floor(Math.random()*12)+1;
+            crystal3 = Math.floor(Math.random()*12)+1;
+            crystal4 = Math.floor(Math.random()*12)+1;
+            random = Math.floor(Math.random()*102)+19;
+            score = 0;
+            $(".rndNum").html(random);
+            $(".score").html(score);
+        }
+    };
 
-    random = Math.floor(Math.random()*102)+19; //sets a random number as the page is loaded
-    $(".rndNum").html(random);
-    crystal1 = Math.floor(Math.random()*12)+1;
-    crystal2 = Math.floor(Math.random()*12)+1;
-    crystal3 = Math.floor(Math.random()*12)+1;
-    crystal4 = Math.floor(Math.random()*12)+1;
+    $(".rndNum").html(random); //loads the randomly generated number to the html
+
     console.log(crystal1, crystal2, crystal3, crystal4); //logs out the numbers for cheaters
 
     $(".crystalImgOne").on("click", function() {
@@ -20,6 +47,7 @@ $(document).ready(function(){
         crystalValue = parseInt(crystalValue);
         score += crystalValue;
         $(".score").html(score);
+        check();
     });
 
         $(".crystalImgTwo").on("click", function() {
@@ -28,6 +56,7 @@ $(document).ready(function(){
         crystalValue = parseInt(crystalValue);
         score += crystalValue;
         $(".score").html(score);
+        check();
     });
 
         $(".crystalImgThree").on("click", function() {
@@ -36,6 +65,7 @@ $(document).ready(function(){
         crystalValue = parseInt(crystalValue);
         score += crystalValue;
         $(".score").html(score);
+        check();
     });
 
         $(".crystalImgFour").on("click", function() {
@@ -44,9 +74,7 @@ $(document).ready(function(){
         crystalValue = parseInt(crystalValue);
         score += crystalValue;
         $(".score").html(score);
+        check();
     });
-
-
-
 }); //closes document.ready
 
