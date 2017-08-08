e1fb2205eca8a2d5661b3163726b24f113f28869
derek-brown <derek.ethan.brown@gmail.com>
2017-08-07 22:41:14 -0400

    Each img generates a random # and the numbers successful add together to the total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,30 +3,50 @@ $(document).ready(function(){
     var crystal2 = -1;
     var crystal3 = -1;
     var crystal4 = -1;
-    var random = -1;
-    console.log(crystal1, crystal2, crystal3, crystal4);
+    var score = 0;
 
 
-    random = Math.floor(Math.random()*102)+19;
+    random = Math.floor(Math.random()*102)+19; //sets a random number as the page is loaded
     $(".rndNum").html(random);
+    crystal1 = Math.floor(Math.random()*12)+1;
+    crystal2 = Math.floor(Math.random()*12)+1;
+    crystal3 = Math.floor(Math.random()*12)+1;
+    crystal4 = Math.floor(Math.random()*12)+1;
+    console.log(crystal1, crystal2, crystal3, crystal4); //logs out the numbers for cheaters
 
-     //sets random number as page is loaded
     $(".crystalImgOne").on("click", function() {
-        crystal1 = Math.floor(Math.random()*12)+1;
-        $(".resultOne").html(crystal1);
+        ($(".score").attr("crystalOne", crystal1));
+        var crystalValue = ($(".score").attr("crystalOne"));
+        crystalValue = parseInt(crystalValue);
+        score += crystalValue;
+        $(".score").html(score);
     });
-    $(".crystalImgTwo").on("click", function() {
-        crystal2 = Math.floor(Math.random()*12)+1;
-        $(".resultTwo").html(crystal2);
+
+        $(".crystalImgTwo").on("click", function() {
+        ($(".score").attr("crystalTwo", crystal2));
+        var crystalValue = ($(".score").attr("crystalTwo"));
+        crystalValue = parseInt(crystalValue);
+        score += crystalValue;
+        $(".score").html(score);
     });
-    $(".crystalImgThree").on("click", function() {
-        crystal3 = Math.floor(Math.random()*12)+1;
-        $(".resultThree").html(crystal3);
+
+        $(".crystalImgThree").on("click", function() {
+        ($(".score").attr("crystalThree", crystal3));
+        var crystalValue = ($(".score").attr("crystalThree"));
+        crystalValue = parseInt(crystalValue);
+        score += crystalValue;
+        $(".score").html(score);
     });
-    $(".crystalImgFour").on("click", function() {
-        crystal4 = Math.floor(Math.random()*12)+1;
-        $(".resultFour").html(crystal4);
+
+        $(".crystalImgFour").on("click", function() {
+        ($(".score").attr("crystalFour", crystal4));
+        var crystalValue = ($(".score").attr("crystalFour"));
+        crystalValue = parseInt(crystalValue);
+        score += crystalValue;
+        $(".score").html(score);
     });
+
+
 
 }); //closes document.ready
 
